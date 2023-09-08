ef2af40c5ac2fd915d09674648e571d8f940c509
Daniel <daniel.benet@outlook.es>
2023-09-08 19:12:44 +0200

    Initial Commit

---
+++ ring-clock/ring_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ring_clockApp.swift
+//  ring-clock
+//
+//  Created by Dani Benet on 8/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ring_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
